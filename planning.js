081b9d3711b32c1ca90f8e15c14820a7a587ee96
Harsha-cpu-prog <harshanenwani121@gmail.com>
2021-09-03 18:58:55 +0530

    HOC and layout

---
+++ planning.js
@@ -0,0 +1,21 @@
+// book my show
+// we need a reusable carousel
+// hero
+// common
+// cast and crew small carousel
+
+
+// pages
+// homepage
+// moviepage
+// categories page
+// auth page
+// razor pay payment gateway
+
+
+// layout
+// routes
+// pages
+// components
+
+// HOC higher order components
